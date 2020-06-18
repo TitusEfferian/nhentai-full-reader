77af878ea5b36218e228bbef9d2a6580517f2328
titusefferian <titus.efferian@tokopedia.com>
2020-06-18 09:43:11 +0700

    change bypass

--- src/endpoints/index.js
+++ src/endpoints/index.js
@@ -1,2 +1,2 @@
 export const nhentaiCrawler = `https://nhentai-crawler-dot-nhentai-unofficial.et.r.appspot.com/source/`;
-export const nhentaiByPass = `https://nhentai-bypass-dot-nhentai-unofficial.et.r.appspot.com?source=`;
+export const nhentaiByPass = `http://34.101.151.150?source=`;
