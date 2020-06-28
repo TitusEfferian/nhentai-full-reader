382c51a9b76effe92d1bb5c024e8430812e0974f
titusefferian <kiritosato@gmail.com>
2020-06-28 11:49:56 +0700

    change endpoint

--- src/endpoints/index.js
+++ src/endpoints/index.js
@@ -1,2 +1,2 @@
-export const nhentaiCrawler = `https://asia-east2-fleet-range-273715.cloudfunctions.net/nhentai-crawler-v2?nhentai_id=`;
+export const nhentaiCrawler = `https://asia-east2-nhentai-unofficial-web.cloudfunctions.net/nhentai-crawler-v2?nhentai_id=`;
 export const nhentaiByPass = `https://asia-east2-nhentai-unofficial-web.cloudfunctions.net/nhentaiByPass?source=`;
